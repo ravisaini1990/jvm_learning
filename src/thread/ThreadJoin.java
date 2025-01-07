6b050e9950ead820ebb80fea6309c25b6fb40e73
Saini Ravi <ravisaini1990@gmail.com>
2025-01-07 11:43:27 +0530

    Thread Join Example

---
+++ src/thread/ThreadJoin.java
@@ -0,0 +1,23 @@
+package thread;
+
+/**
+ * Thread Join
+ */
+public class ThreadJoin {
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(() -> {
+            System.out.println("Thread >> "+Thread.currentThread());
+        } , "Thread1");
+
+        t1.start();
+
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Main Thread Exit");
+    }
+}
